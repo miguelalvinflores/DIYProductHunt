ea1618dffd0bdcfd92f0cff1f43c68441ec9a6ad
miguel <miguelalvinflores@gmail.com>
2021-04-05 14:09:40 -0700

    register form validators completed

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,80 @@
 var express = require('express');
 var router = express.Router();
 const { asyncHandler, csrfProtection } = require('./utils');
+const { User } = require('../db/models')
+const {check, validationResult } = require('express-validator')
 
 /* GET users listing. */
 router.get('/signup', csrfProtection, asyncHandler(async(req, res, next) => {
-  const user = await db.User.build();
+  const user = await User.build();
   res.render("user-signup", {
     title: "Sign Up",
     user,
     csrfToken: req.csrfToken()
   })
-
 }));
+const userValidators = [
+  check("emailAddress")
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email')
+    .isLength({ max: 100 })
+    .withMessage("Email must be 100 characters or less.")
+    .custom( (value) => {
+      return User.findOne({ where: { emailAddress: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('This email address is already in use, please provide another.')
+          }
+        });
+    }),
+  check("userName")
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a username')
+    .isLength({ max: 25 })
+    .withMessage("Username must be 25 characters or less.")
+    .custom( (value) => {
+      return User.findOne({ where: { userName: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('This username already exist!!')
+          }
+        });
+    }),
+  check("firstName")
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a First mame.')
+    .isLength({ max: 30 })
+    .withMessage("First name must be 30 characters or less."),
+  check("lastName")
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a Last mame.')
+    .isLength({ max: 30 })
+    .withMessage("Last name must be 30 characters or less."),
+  check("profilePicURL")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a image URL")
+    .matches(/^.+[jpg png svg]$/)
+    .withMessage("Must be a link to a valid file format (.jpg, .png, .svg)."),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+    .isLength({ max: 50 })
+    .withMessage('Password must not be more than 50 characters long')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+    .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Confirm Password')
+    .isLength({ max: 50 })
+    .withMessage('Confirm Password must not be more than 50 characters long')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Confirm Password does not match Password');
+      }
+      return true;
+    }),
+]
 
+router.post('')
 module.exports = router;
