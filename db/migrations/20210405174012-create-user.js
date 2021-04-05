34349196fc559a7be262377d317de84edae77fc9
miguel <miguelalvinflores@gmail.com>
2021-04-05 10:50:32 -0700

    created users sequelize database

---
+++ db/migrations/20210405174012-create-user.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      emailAddress: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true
+      },
+      userName: {
+        type: Sequelize.STRING(25),
+        unique: true,
+        allowNull: false,
+      },
+      firstName: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      profilePicURL: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      hashedPW: {
+        type: Sequelize.STRING.BINARY,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
---
+++ db/models/user.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    emailAddress: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true
+    },
+    userName: {
+      type: DataTypes.STRING(25),
+      unique: true,
+      allowNull: false,
+    },
+    firstName: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    profilePicURL: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    hashedPW: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+    },
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
