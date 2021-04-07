1bd4398bffdcc10ab755d8cd47243f0ae6c92952
Laura <lkzailac@gmail.com>
2021-04-07 17:11:58 -0400

    working on about

---
+++ routes/about.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const { loginUser, logoutUser, requireAuth, restoreUser } = require('../auth');
+
+
+router.get('/', restoreUser, (req,res) => {
+    res.render('about', {title: 'About'})
+})
+
+
+module.exports = router;
