53f50f974893dbb5d11122764319c83efa0530c5
Ben Stetzer <benstetzer@gmail.com>
2021-04-08 16:55:07 -0400

    starting on delete profile

--- public/javascripts/user-profile.js
+++ public/javascripts/user-profile.js
@@ -6,21 +6,22 @@ document.addEventListener("DOMContentLoaded", async () => {
     const editProfileBtn = document.querySelector('.edit-profile-btn')
     const editProfileBtnCancel = document.querySelector('.edit-btn-cancel')
 
-    const modal = document.querySelector('.modal')
+    const modalEditProfile = document.querySelector('#modal-edit-profile')
     editProfileBtn.addEventListener('click', (e) => {
         e.preventDefault();
-        modal.style.display = "block";
+        modalEditProfile.style.display = "block";
     })
     editProfileBtnCancel.addEventListener('click', (e) => {
         e.preventDefault();
-        modal.style.display = "none";
+        modalEditProfile.style.display = "none";
     })
 
     const editBtn = document.querySelector('.edit-btn');
     // const userIdnum = parseInt(userId, 10);
     editBtn.addEventListener("click", async (e) => {
-            e.preventDefault();
-
+        e.preventDefault();
+        let editProfileErrors = document.querySelector('#edit-profile-errors')
+        editProfileErrors.innerHTML = '';
         let userId = document.querySelector('#user-id').value;
         let firstName = document.querySelector('#firstName').value;
         let lastName = document.querySelector('#lastName').value;
@@ -45,25 +46,47 @@ document.addEventListener("DOMContentLoaded", async () => {
             body: JSON.stringify(body)
         })
         const jsonUser = await updatedUser.json();
-        const {
-            firstName: firstNameU,
-            lastName : lastNameU,
-            userName: userNameU,
-            emailAddress: emailAddressU,
-            profilePicURL: profilePicURLU
-        } = jsonUser.user
-        console.log(jsonUser)
-
-        const fullname = document.querySelector('#full-name');
-        const contact = document.querySelector('#contact');
-        const image = document.querySelector('#image');
-        const username = document.querySelector('#username');
-
-
-        modal.style.display = "none";
-        fullname.innerHTML = `${firstNameU} ${lastNameU}`
-        contact.innerHTML = `Contact Creator: ${emailAddressU}`
-        image.src = profilePicURLU
-        username.innerHTML = `Username: ${userNameU}`
+        if (jsonUser.user) {
+            const {
+                firstName: firstNameU,
+                lastName : lastNameU,
+                userName: userNameU,
+                emailAddress: emailAddressU,
+                profilePicURL: profilePicURLU
+            } = jsonUser.user
+            console.log(jsonUser)
+    
+            const fullname = document.querySelector('#full-name');
+            const contact = document.querySelector('#contact');
+            const image = document.querySelector('#image');
+            const username = document.querySelector('#username');
+    
+    
+            modalEditProfile.style.display = "none";
+            fullname.innerHTML = `${firstNameU} ${lastNameU}`
+            contact.innerHTML = `Contact Creator: ${emailAddressU}`
+            image.src = profilePicURLU
+            username.innerHTML = `Username: ${userNameU}`
+            
+        } else if (jsonUser.userNotFound) {
+            alert('This account does not exist. You are now being redirected to the home page');
+            console.log('redirected')
+            window.location.href = 'http://localhost:8080/'
+        } else {
+            console.log(jsonUser.errors)
+            const errorDiv = document.createElement('div');
+            const errorP = document.createElement('p');
+            errorP.innerHTML = 'Please correct the following:'
+            const errorList = document.createElement('ul')
+            errorDiv.appendChild(errorP);
+            errorDiv.appendChild(errorList)
+            jsonUser.errors[0].forEach(error => {
+                let errorItem = document.createElement('li')
+                errorItem.innerHTML = error;
+                errorList.appendChild(errorItem)
+                console.log(errorItem)
+            });
+            editProfileErrors.appendChild(errorDiv)
+        }
     })
 })
--- routes/api.js
+++ routes/api.js
@@ -5,82 +5,10 @@ const { asyncHandler, csrfProtection } = require('./utils');
 const { User, Product } = require('../db/models')
 const { check, validationResult } = require('express-validator');
 const { loginUser, logoutUser, requireAuth, restoreUser } = require('../auth');
+const userApi = require('./api-users')
 
-const userValidators = [
-    check("emailAddress")
-        .exists({ checkFalsy: true })
-        .isEmail()
-        .withMessage('Please provide a valid email')
-        .isLength({ max: 100 })
-        .withMessage("Email must be 100 characters or less."),
-    check("userName")
-        .exists({ checkFalsy: true })
-        .withMessage('Please provide a username')
-        .isLength({ max: 25 })
-        .withMessage("Username must be 25 characters or less."),
-    check("firstName")
-        .exists({ checkFalsy: true })
-        .withMessage('Please provide a First mame.')
-        .isLength({ max: 30 })
-        .withMessage("First name must be 30 characters or less."),
-    check("lastName")
-        .exists({ checkFalsy: true })
-        .withMessage('Please provide a Last mame.')
-        .isLength({ max: 30 })
-        .withMessage("Last name must be 30 characters or less."),
-    check("profilePicURL")
-        .exists({ checkFalsy: true })
-        .withMessage("Please provide a image URL")
-        .matches(/^.+[jpe?g png svg]$/)
-        .withMessage("Must be a link to a valid file format (.jpg, .png, .svg)."),
-];
+router.use('/users', userApi)
 
-router.put('/users/:id(\\d+$\)', userValidators,
-    asyncHandler( async( req, res) => {
-        const validatorErrors = validationResult(req);
-        const errors = [];
-        if (validatorErrors.isEmpty()) {
-            const id = parseInt(req.params.id, 10);
-            const user = await User.findOne({ where: { id } });
-            if(user) {
-                const {
-                    firstName,
-                    lastName,
-                    userName,
-                    emailAddress,
-                    profilePicURL,
-                    password
-                } = req.body;
-
-
-                const passwordMatch = await bcrypt.compare(password, user.hashedPW.toString());
-                console.log(passwordMatch)
-
-                if (passwordMatch) {
-                    const updatedUser = await user.update({
-                        firstName,
-                        lastName,
-                        userName,
-                        emailAddress,
-                        profilePicURL
-                    });
-                    console.log(updatedUser)
-                    res.json({user})
-                } else {
-                    errors.push('That password does not match our records.')
-                    res.json({errors})
-                }
-            } else {
-                res.redirect('/users/login')
-                res.render('user-profile', { title: `${user.firstName}`, errors, csrfToken: req.csrfToken() })
-            }
-        } else {
-            errors.push(validatorErrors.array().map((error) => error.msg));
-        }
-
-        // res.render('user-profile', { title: `${user.firstName}`, errors, csrfToken: req.csrfToken() })
-    }
-    ))
 
 
 module.exports = router;
